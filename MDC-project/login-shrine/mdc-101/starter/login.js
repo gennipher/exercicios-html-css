174c23389ceda6a27e7bcc85f1129755879a0b61
Jennifer Omena <jennifer.omena@m4u.com.br>
2019-05-03 19:24:29 -0300

    Push nos Arquivos

---
+++ MDC-project/login-shrine/mdc-101/starter/login.js
@@ -0,0 +1,11 @@
+import {MDCTextField} from '@material/textfield';
+
+const username = new MDCTextField(document.querySelector('.username'));
+const password = new MDCTextField(document.querySelector('password'));
+
+import {MDCRipple} from '@material/ripple';
+
+new MDCRipple(document.querySelector('.cancel'));
+new MDCRipple(document.querySelector('.next'));
+
+
---
+++ MDC-project/login-shrine/mdc-102/starter/home.js
@@ -0,0 +1,2 @@
+import {MDCList} from '@material/list';
+new MDCList(document.querySelector('.mdc-list'));
